27eddd58d06f628a2cf53c8e3cfc7037c7ad5e37
James Isaac <chizuruokej@yahoo.com>
2020-09-24 13:33:21 +0100

    new commit

--- src/component/Transaction.js
+++ src/component/Transaction.js
@@ -1,10 +1,9 @@
 import React from 'react'
 
 export const Transaction = ({transactions}) => {
-    transactions.map(transaction => transaction.amount)
     return (
         <li className='transaction-container'>
-            Cash<span>NGN50,000</span><button className= 'delete-btn'>x</button>    
+            Cash<span>NGN{}</span><button className= 'delete-btn'>x</button>    
         </li>  
     )
 }
